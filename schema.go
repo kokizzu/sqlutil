0d8acaec9069a2c80ab72ac9b8005d6e83441a98
Svetlin Ralchev <svet@ralch.com>
2017-03-09 21:03:32 +0000

    Create table from struct

---
+++ schema.go
@@ -0,0 +1,46 @@
+package sqlutil
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+const Separator = ",\n"
+
+func CreateTable(db *sql.DB, model interface{}) error {
+	t, err := typeOf(model)
+	if err != nil {
+		return err
+	}
+
+	schema, err := metadata.Schema(t)
+	if err != nil {
+		return err
+	}
+
+	definitions := []string{}
+	tableName := strings.ToLower(t.Name())
+
+	for _, column := range schema.Columns {
+		definition := fmt.Sprintf(" %s %s %s", column.Name, column.DataType, column.Constraint.String())
+		definition = strings.TrimRight(definition, " ")
+		definitions = append(definitions, definition)
+	}
+
+	definitions = append(definitions, fmt.Sprintf(" CONSTRAINT %s_pk PRIMARY KEY(%s)", tableName, strings.Join(schema.PrimaryKey, Separator)))
+	statement := fmt.Sprintf("CREATE TABLE %s (\n%s\n)", tableName, strings.Join(definitions, Separator))
+
+	if _, err = db.Exec(statement); err != nil {
+		return err
+	}
+
+	for _, index := range schema.Indexes {
+		statement := fmt.Sprintf("CREATE INDEX %s ON %s (%s)", index.Name, tableName, strings.Join(index.Columns, ","))
+		if _, err := db.Exec(statement); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ schema_test.go
@@ -0,0 +1,72 @@
+package sqlutil_test
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/phogolabs/sqlutil"
+
+	_ "github.com/mattn/go-sqlite3"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Schema", func() {
+	var db *sql.DB
+
+	BeforeEach(func() {
+		var err error
+		db, err = sql.Open("sqlite3", "sqlutil.db")
+		Expect(err).To(BeNil())
+	})
+
+	AfterEach(func() {
+		_, err := db.Exec("drop table m")
+		Expect(err).To(BeNil())
+		Expect(db.Close()).To(Succeed())
+	})
+
+	It("creates a table successfully", func() {
+		type m struct {
+			ID        string    `sql:"id,varchar(50),pk"`
+			Name      string    `sql:"name,text,not_null,unique,index"`
+			CreatedAt time.Time `sql:"created_at,timestamp,null"`
+		}
+
+		Expect(sqlutil.CreateTable(db, &m{})).To(Succeed())
+
+		rows, err := db.Query("pragma table_info(m)")
+		Expect(err).To(BeNil())
+		defer rows.Close()
+
+		var (
+			no           int
+			name         string
+			dataType     string
+			notNull      int
+			defaultValue interface{}
+			isPK         int
+		)
+
+		Expect(rows.Next()).To(BeTrue())
+		Expect(rows.Scan(&no, &name, &dataType, &notNull, &defaultValue, &isPK)).To(Succeed())
+		Expect(name).To(Equal("id"))
+		Expect(dataType).To(Equal("varchar(50)"))
+		Expect(notNull).To(Equal(0))
+		Expect(isPK).To(Equal(1))
+
+		Expect(rows.Next()).To(BeTrue())
+		Expect(rows.Scan(&no, &name, &dataType, &notNull, &defaultValue, &isPK)).To(Succeed())
+		Expect(name).To(Equal("name"))
+		Expect(dataType).To(Equal("text"))
+		Expect(notNull).To(Equal(1))
+		Expect(isPK).To(Equal(0))
+
+		Expect(rows.Next()).To(BeTrue())
+		Expect(rows.Scan(&no, &name, &dataType, &notNull, &defaultValue, &isPK)).To(Succeed())
+		Expect(name).To(Equal("created_at"))
+		Expect(dataType).To(Equal("timestamp"))
+		Expect(notNull).To(Equal(0))
+		Expect(isPK).To(Equal(0))
+	})
+})
