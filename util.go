70964cbb8906999c3de39893f8bda4a0b0468926
Svetlin Ralchev <svet@ralch.com>
2017-03-12 17:24:51 +0000

    Move CRUD operations to util

--- util.go
+++ util.go
@@ -2,6 +2,22 @@ package sqlutil
 
 import "database/sql"
 
+func QueryRow(db *sql.DB, model interface{}) error {
+	return Entity(model).QueryRow(db)
+}
+
+func Insert(db *sql.DB, model interface{}) (int64, error) {
+	return Entity(model).Insert(db)
+}
+
+func Update(db *sql.DB, model interface{}) (int64, error) {
+	return Entity(model).Update(db)
+}
+
+func Delete(db *sql.DB, model interface{}) (int64, error) {
+	return Entity(model).Delete(db)
+}
+
 func execSQL(db *sql.DB, statement string, values ...interface{}) (int64, error) {
 	result, err := db.Exec(statement, values...)
 	if err != nil {
